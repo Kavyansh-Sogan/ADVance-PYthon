840b902be79e2784571a346539767b163d6c1e8e
Kavyansh Sogan <kavyanshsogan@gmail.com>
2026-09-01 19:37:28 +0530

    ADDED

---
+++ basic.py
@@ -0,0 +1,11 @@
+a=0
+b=0
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+if a>b:
+    print("a is greater than b")
+elif b>a:
+    print("b is greater than a")
+else:
+    print("a is equal to b")
+    
